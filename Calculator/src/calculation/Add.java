062ff98763b6020ea84f743c86ea8c4ddf2175fc
janhvi kale <janhvikale30@gmail.com>
2026-09-23 15:15:30 +0530

    Developed four calculator operation

---
+++ Calculator/src/calculation/Add.java
@@ -0,0 +1,28 @@
+package calculation;
+
+public class Add {
+	public int addition(int a, int b) {
+		int res = a+b;
+		return res;
+	}
+	public int subtraction(int a, int b) {
+		int res = a-b;
+		return res;
+	}
+	public int multiplication(int a, int b) {
+		int res = a*b;
+		return res;
+	}
+	public int division(int a, int b) {
+		int res = a/b;
+		return res;
+	}
+	public static void main(String[] args) {
+		Add a = new Add();
+		System.out.println("Addition :"+a.addition(12,67));
+		System.out.println("Subtraction :"+a.subtraction(45, 90));
+		System.out.println("Multiplication :"+a.division(54, 4));
+		System.out.println("Division :"+a.division(54, 9));
+	}
+
+}
